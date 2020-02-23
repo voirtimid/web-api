7db2e1bb58852a4d07ac5cc65519fe31cf9ae35d
voirtimid <oliver.dimitriov@gmail.com>
2020-02-23 23:52:56 +0100

    updated FileUploadController to read content from files

--- src/main/java/mk/metalkat/webapi/controller/FileUploadController.java
+++ src/main/java/mk/metalkat/webapi/controller/FileUploadController.java
@@ -1,11 +1,15 @@
 package mk.metalkat.webapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import mk.metalkat.webapi.models.Cnc;
 import mk.metalkat.webapi.service.CNCService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,48 +28,61 @@ public class FileUploadController {
     private final CNCService cncService;
 
     @PostMapping("/{folderName}")
-    public boolean singleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable("folderName") String folderName) {
+    public String singleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable("folderName") String folderName) {
 
         if (file.isEmpty()) {
-            return false;
+            return "false";
         }
 
         try {
 
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + folderName + "/" + file.getOriginalFilename());
+            String filepath = UPLOADED_FOLDER + folderName + "/";
+            File f = new File(filepath);
+            if (Files.notExists(Paths.get(filepath))) {
+                boolean mkdirs = f.mkdirs();
+                if (!mkdirs) {
+                    return "false";
+                }
+            }
+
+            Path path = Paths.get(filepath + file.getOriginalFilename());
             Files.write(path, bytes);
 
-            return true;
+            return new String(bytes);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return false;
+        return "false";
     }
 
-    @PostMapping("/cnc/{cncId}")
-    public boolean singleFileUploadCNC(@RequestParam("file") MultipartFile file, @PathVariable("cncId") Long cncId) {
-
-        if (file.isEmpty()) {
-            return false;
-        }
-
+    @GetMapping("/{cncId}")
+    public String readFileFromDisk(@PathVariable("cncId") Long cncId) {
         try {
 
-            // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+            Cnc cnc = cncService.getCNC(cncId);
 
-            return true;
+            String filepath = UPLOADED_FOLDER + "cnc/" + cnc.getCncFilename();
+            // Get the file and save it somewhere
+
+            File file = new File(filepath);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            return sb.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return false;
+        return "false";
     }
 }
